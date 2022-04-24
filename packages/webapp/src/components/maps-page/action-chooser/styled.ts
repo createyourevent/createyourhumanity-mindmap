f674330797f0482cd8c6b7b7c86633c51e1ebeda
Manuel Miklo <manuel@createyourevent.org>
2022-04-24 16:00:31 +0200

    delete webfolder

--- packages/webapp/src/components/maps-page/action-chooser/styled.ts
+++
@@ -1,8 +0,0 @@
-import MenuItem from '@mui/material/MenuItem';
-import withStyles from '@mui/styles/withStyles';
-
-export const StyledMenuItem = withStyles({
-    root: {
-        width: '300px',
-    },
-})(MenuItem);
